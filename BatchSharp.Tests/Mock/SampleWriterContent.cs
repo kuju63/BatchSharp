a640c62e78370cdc45fffcb628618a55c8fcc4cc
Jun Kurihara <lh182051+src@gmail.com>
2023-06-15 22:18:35 +0900

    fix format error

--- BatchSharp.Tests/Mock/SampleWriterContent.cs
+++ BatchSharp.Tests/Mock/SampleWriterContent.cs
@@ -1,7 +1,11 @@
 namespace BatchSharp.Tests.Mock;
 
+/// <summary>
+/// Sample writer content
+/// </summary>
 public class SampleWriterContent
 {
+    /// <inheritdoc cref="object.ToString"/>
     public override string ToString()
     {
         return "test";
