feb83cd4a6cbacae85bb735dd4a633f78a9cc9dc
Jun Kurihara <lh182051+src@gmail.com>
2023-07-17 22:49:40 +0900

    implement execution

---
+++ BatchSharp.Tests/Step/SimpleStepTest.cs
@@ -0,0 +1,107 @@
+using BatchSharp.Processor;
+using BatchSharp.Reader;
+using BatchSharp.Step;
+using BatchSharp.Writer;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+namespace BatchSharp.Tests.Step;
+
+/// <summary>
+/// Test class for <see cref="SimpleStep{T1,T2}"/>.
+/// </summary>
+public class SimpleStepTest : IDisposable
+{
+    private readonly Mock<IReader<string>> _reader = new();
+    private readonly Mock<IProcessor<string, int>> _processor = new();
+    private readonly Mock<IWriter<int>> _writer = new();
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    private readonly Mock<ILogger<SimpleStep<string, int>>> _logger = new();
+
+    /// <summary>
+    /// Release resources.
+    /// </summary>
+    public void Dispose()
+    {
+        _cancellationTokenSource.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Should return canceled task.
+    /// </summary>
+    [Fact]
+    public async void ShouldReturnCanceledTask()
+    {
+        _reader.Setup(r => r.ReadAsync());
+        _processor.Setup(p => p.Process(It.IsAny<string>()));
+        _writer.Setup(x => x.WriteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()));
+        _cancellationTokenSource.Cancel();
+
+        var step = new SimpleStep<string, int>(_logger.Object, _reader.Object, _processor.Object, _writer.Object);
+
+        await Assert.ThrowsAsync<TaskCanceledException>(async () => await step.ExecuteAsync(_cancellationTokenSource.Token));
+
+        _reader.Verify((r) => r.ReadAsync(), Times.Never());
+        _processor.Verify(p => p.Process(It.IsAny<string>()), Times.Never());
+        _writer.Verify(x => x.WriteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()), Times.Never());
+    }
+
+    /// <summary>
+    /// Test for <see cref="SimpleStep{T1,T2}.ExecuteAsync(CancellationToken)"/>.
+    /// If reader returns single element list, should return completed.
+    /// </summary>
+    /// <returns>Asynchronous task.</returns>
+    [Fact]
+    public async Task ShouldReturnCompletedWhenReadSingleAsync()
+    {
+        _reader.SetupSequence(x => x.ReadAsync())
+            .Returns(new[] { "test" }.ToAsyncEnumerable())
+            .Returns(AsyncEnumerable.Empty<string>());
+
+        _processor.SetupSequence(x => x.Process("test"))
+            .Returns(4);
+        _writer.SetupSequence(x => x.WriteAsync(It.IsIn(4), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        var step =
+            new SimpleStep<string, int>(
+                _logger.Object,
+                _reader.Object,
+                _processor.Object,
+                _writer.Object);
+
+        await step.ExecuteAsync(_cancellationTokenSource.Token);
+
+        _reader.Verify((r) => r.ReadAsync(), Times.Once());
+        _processor.Verify(p => p.Process("test"), Times.Once());
+        _writer.Verify(x => x.WriteAsync(It.IsIn(4), It.IsAny<CancellationToken>()), Times.Once());
+    }
+
+    /// <summary>
+    /// Test for <see cref="DefaultBatchApplication{TRead,TResult}.RunAsync()"/>.
+    /// Should return completed when reader returns empty list.
+    /// </summary>
+    /// <returns>Asynchronous task.</returns>
+    [Fact]
+    public async Task ShouldReturnCompletedWhenReadEmptyAsync()
+    {
+        _reader.SetupSequence(x => x.ReadAsync())
+            .Returns(AsyncEnumerable.Empty<string>());
+        _processor.SetupSequence(x => x.Process(It.IsAny<string>()));
+        _writer.SetupSequence(x => x.WriteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()));
+        var step =
+            new SimpleStep<string, int>(
+                _logger.Object,
+                _reader.Object,
+                _processor.Object,
+                _writer.Object);
+
+        await step.ExecuteAsync(_cancellationTokenSource.Token);
+
+        _reader.Verify(r => r.ReadAsync(), Times.Once());
+        _processor.Verify(x => x.Process(It.IsAny<string>()), Times.Never());
+        _writer.Verify(x => x.WriteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()), Times.Never());
+    }
+}
--- BatchSharp/Step/SimpleStep.cs
+++ BatchSharp/Step/SimpleStep.cs
@@ -2,6 +2,8 @@ using BatchSharp.Processor;
 using BatchSharp.Reader;
 using BatchSharp.Writer;
 
+using Microsoft.Extensions.Logging;
+
 namespace BatchSharp.Step;
 
 /// <summary>
@@ -17,18 +19,21 @@ public class SimpleStep<T1, T2> : IStep<T1, T2>, IDisposable
     private readonly IReader<T1> _reader;
     private readonly IProcessor<T1, T2> _processor;
     private readonly IWriter<T2> _writer;
+    private readonly ILogger<SimpleStep<T1, T2>> _logger;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SimpleStep{T1, T2}"/> class.
     /// </summary>
+    /// <param name="logger">Instance of logger.</param>
     /// <param name="reader">Instance of reader class for load the data from data source.</param>
     /// <param name="processor">Instance of processor.</param>
     /// <param name="writer">Instance of writer for output processor result.</param>
-    public SimpleStep(IReader<T1> reader, IProcessor<T1, T2> processor, IWriter<T2> writer)
+    public SimpleStep(ILogger<SimpleStep<T1, T2>> logger, IReader<T1> reader, IProcessor<T1, T2> processor, IWriter<T2> writer)
     {
         _reader = reader;
         _processor = processor;
         _writer = writer;
+        _logger = logger;
     }
 
     /// <summary>
@@ -41,8 +46,21 @@ public class SimpleStep<T1, T2> : IStep<T1, T2>, IDisposable
     }
 
     /// <inheritdoc cref="IStep" />
-    public Task ExecuteAsync(CancellationToken cancellationToken)
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!cancellationToken.IsCancellationRequested)
+        {
+            await foreach (var readData in _reader.ReadAsync().WithCancellation(cancellationToken))
+            {
+                _logger.LogInformation("Read data: {Item}", readData);
+                var processingResult = _processor.Process(readData);
+                _logger.LogInformation("Processed data: {Item}", processingResult);
+                await _writer.WriteAsync(processingResult, cancellationToken);
+            }
+        }
+        else
+        {
+            await Task.FromCanceled(cancellationToken);
+        }
     }
 }
